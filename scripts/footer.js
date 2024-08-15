cc23ba2ec9999c8f671a8dc66c4eb937caf9ff89
Sharky83 <alex_brocky@hotmail.com>
2024-08-15 12:42:06 +0100

    moved header and footer into js files

---
+++ scripts/footer.js
@@ -0,0 +1,23 @@
+let appFooter = `
+     <div class="container main-footer-container">
+        <h3 class="shop-name">The Shroomies</h3>
+        <ul class="nav footer-nav">
+          <li>
+            <a href="https://www.youtube.com" target="_blank">
+              <img src="./images/Youtube-Logo.png" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.spotify.com" target="_blank">
+              <img src="./images/Spotify-Logo.png" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com" target="_blank">
+              <img src="./images/Facebook-Logo.png" />
+            </a>
+          </li>
+        </ul>
+      </div>
+`;
+document.getElementById("app-footer").innerHTML = appFooter;
---
+++ scripts/header.js
@@ -0,0 +1,14 @@
+let appHeader = `
+     <nav class="nav">
+                <ul>
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./guides.html">Guides</a></li>
+                    <li><a href="./store.html">Store</a></li>
+                    <li><a href="./about.html">About</a></li>
+                    <li><a href="./blog.html">Blog</a></li>
+                </ul>
+            </nav>
+        <h1 class="shop-name">The Shroomies</h1>
+        <button type="button" class="btn btn-header" id="cart-button">Basket</button>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
