e7d9a048c6ee8f11925994b707f400a88a95d34b
Sharky83 <alex_brocky@hotmail.com>
2024-08-15 15:46:47 +0100

    footer to bottom on all pages, fixed broken links

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,28 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const footer = document.getElementById('app-footer');
-
-  const footerContent = `
-      <div class="container">
-          <h3 class="shop-name">The Shroomies</h3>
-          <ul class="footer-nav">
-              <li>
-                  <a href="https://www.youtube.com" target="_blank">
-                      <img src="./images/Youtube-Logo.png">
-                  </a>
-              </li>
-              <li>
-                  <a href="https://www.spotify.com" target="_blank">
-                      <img src="./images/Spotify-Logo.png">
-                  </a>
-              </li>
-              <li>
-                  <a href="https://www.facebook.com" target="_blank">
-                      <img src="./images/Facebook-Logo.png">
-                  </a>
-              </li>
-          </ul>
-      </div>
-  `;
-
-  footer.innerHTML = footerContent;
-});
+    const footer = document.getElementById('app-footer');
+  
+    const footerContent = `
+        <div class="container footer-container">
+            <h3 class="shop-name">The Shroomies</h3>
+            <ul class="footer-nav">
+                <li>
+                    <a href="https://www.youtube.com" target="_blank">
+                        <img src="./images/Youtube-Logo.png">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.spotify.com" target="_blank">
+                        <img src="./images/Spotify-Logo.png">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com" target="_blank">
+                        <img src="./images/Facebook-Logo.png">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    `;
+  
+    footer.innerHTML = footerContent;
+  });
+  
